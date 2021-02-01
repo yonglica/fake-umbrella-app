c2af36fbd38a2a16d65d710d2179b5ea553f87cf
Yong Li <liyong2005@gmail.com>
2021-01-31 23:03:20 -0500

    config database connection in environment file, add redis in docker compose file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module, HttpModule } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RedisCacheModule } from './redis-cache/redis-cache.module';
 import { AppController } from './app.controller';
@@ -7,18 +7,20 @@ import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
 import { ForecastModule } from './forecast/forecast.module';
 
-// const DB_URL = 'mongodb://database:27017/customers';    // Docker
-const DB_URL = 'mongodb://localhost/customers';         // local
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot(DB_URL, {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('DB_URL'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+     }),
     HttpModule,
     CustomerModule,
     ForecastModule,
